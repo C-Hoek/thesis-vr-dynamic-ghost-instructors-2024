c0ff1ba86f589a907949cfcd9989713197d00511
cassandravisse <c.m.s.visser@student.tudelft.nl>
2024-05-22 13:27:20 +0200

    Add a skeleton for the task classes

--- Assets/Scripts/Task/ITask.cs
+++ Assets/Scripts/Task/ITask.cs
@@ -3,18 +3,42 @@ namespace Task
     public interface ITask
     {
         /// <summary>
-        /// 
+        /// Implementations of this method should show the vertices of the task to the student.
+        /// Edges should not be shown to the student.
+        /// </summary>
+        public void VisualiseTask();
+
+        /// <summary>
+        /// Implementations of this method should show the vertices, edges, and helpful instructions to the Ghost.
+        /// </summary>
+        public void VisualiseTaskGhost();
+
+        /// <summary>
+        /// Implementations of this method should return a boolean that represents if the pressed vertex is the correct next vertex.
+        /// </summary>
+        /// <param name="v"> The pressed vertex. </param>
+        /// <param name="previous"> The vertex that the user pressed previously. </param>
+        /// <returns></returns>
+        public bool IsVertexCorrect(GameObject v, GameObject previous);
+
+        /// <summary>
+        /// Implementations of this method should mark a vertex as pressed.
+        /// Correct vertices should be marked visually differently from incorrect vertices.
+        /// </summary>
+        /// <param name="v"> The pressed vertex. </param>
+        public void MarkVertexPressed(GameObject v);
+
+        /// <summary>
+        /// Implementations of this method should mark a vertex as unpressed.
+        /// This should be called to remove incorrectly pressed vertices after visual feedback has shown the student that it is wrong.
+        /// </summary>
+        /// <param name="v"> The vertex that should be marked as unpressed. </param>
+        public void MarkVertexUnpressed(GameObject v);
+
+        /// <summary>
+        /// Implementations of this method should check whether the task has been fully and correctly completed.
         /// </summary>
         /// <returns> True if the task has been completed. False otherwise. </returns>
         public bool IsComplete();
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        public TaskDetails TaskDetails();
-
-
-
     }
 }
