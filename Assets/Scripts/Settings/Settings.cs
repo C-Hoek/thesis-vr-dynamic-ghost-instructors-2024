18cd54717a22a6a169cfc6232b82f6dada3ea026
C.M.S. Visser <c.m.s.visser@student.tudelft.nl>
2024-05-23 14:29:07 +0200

    Fix compiler error in settings

--- Assets/Scripts/Settings/Settings.cs
+++ Assets/Scripts/Settings/Settings.cs
@@ -2,9 +2,9 @@ namespace Settings
 {
     public class Settings
     {
-        public const float MinimumGhostTransparency;
-        public const float MaximumGhostTransparency;
-        public const float BaseGhostTransparency;
+        public float MinimumGhostTransparency;
+        public float MaximumGhostTransparency;
+        public float BaseGhostTransparency;
 
 
         public void LoadSettings()
